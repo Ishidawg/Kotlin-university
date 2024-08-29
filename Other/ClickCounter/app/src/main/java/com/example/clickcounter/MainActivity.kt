82fda14446c0321e393a18b53e8b8698101eab03
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-08-29 17:30:28 -0300

    add image meme

--- Other/ClickCounter/app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ Other/ClickCounter/app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.clickcounter
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +22,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val myTitle = findViewById<TextView>(R.id.maintxt);
         val myButton = findViewById<TextView>(R.id.clickbtn);
         var myResult = findViewById<TextView>(R.id.result);
+        var myBonus = findViewById<TextView>(R.id.bonus);
+        var myImage = findViewById<ImageView>(R.id.memeImage);
+
+        val bonuses = arrayOf("STOP!", "DAMN BOY, STOP!!", "YOU'LL BREAKE YOUR SCREEN!!", "JESUS!!");
 
         var count = 0
 
+        fun randomBonuses() {
+            val randomValues = List(3) { Random.nextInt(bonuses.size) };
+            val randomIndex = randomValues.random();
+            myBonus.setText(bonuses[randomIndex]);
+        }
+
+        fun checkBunus() {
+            if (count <= 19) {
+                myBonus.setText("");
+            } else if (count <= 20) {
+                myBonus.setText("Wow, you've clicked $count times..");
+            } else if (count >= 21 && count < 50) {
+                myBonus.setText("Wow, you've clicked $count times!#@%$#!!");
+            } else {
+                randomBonuses()
+                myImage.setImageResource(R.drawable.imagememe)
+            }
+        }
+
         myButton.setOnClickListener{
-            myResult.setText(count.toString())
-            count += 1
+            myResult.setText(count.toString());
+            count += 1;
+
+            checkBunus();
         }
     }
 }
